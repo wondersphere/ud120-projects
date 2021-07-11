d52d391f836a4863dd2da16ea2f1fbda12dd3e90
wondersphere <70729766+wondersphere@users.noreply.github.com>
2021-07-11 23:48:19 +0200

    commit on 'master'

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -12,9 +12,13 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
+#sys.path.append("../tools/")
 
+import os
+
+sys.path.append(os.path.join(os.getcwd(), "tools"))
+
+from email_preprocess import preprocess
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -26,6 +30,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+gnb_classifier = GaussianNB()
+
+t0 = time()
+gnb_classifier.fit(features_train, labels_train)
+t_train = round(time()-t0, 3)
+
+t0 = time()
+pred = gnb_classifier.predict(features_test)
+t_predict = round(time()-t0, 3)
+
+accuracy = accuracy_score(labels_test, pred)
+print(accuracy)
+
+print "training time:", t_train, "s"
+print "prediction time:", t_predict, "s"
 
 
 #########################################################
